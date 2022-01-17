ca097469f86cd4652259bcddec96589946e2781f
Ricardo Costa <ra.silva.dcosta@gmail.com>
2022-01-17 19:46:11 +0100

    created the generator view, home html and the password html

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import random
+import string
 
-# Create your views here.
+def home(request):
+    return render(request, 'generator/home.html', {'password': 'ergege54654'})
+
+def password(request):
+    password = ''
+    characters = list(string.ascii_lowercase) # string.ascii_lowercase return this 'abcdefghijklmnopqrstuvwxyz'
+
+    if (request.GET.get('uppercase')):
+        characters.extend(list(string.ascii_uppercase))
+    
+    if (request.GET.get('specialcharacters')):
+        characters.extend(list('@$%&*()!'))
+
+    if (request.GET.get('numbers')):
+        characters.extend(list(string.digits))
+    
+    length = int(request.GET.get('length', 12))
+
+    for x in range(length):
+        password += random.choice(characters)
+
+    return render(request, 'generator/password.html', { 'password': password })
